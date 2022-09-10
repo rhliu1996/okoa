80606cb02b8877434640f1725b68a8b166e68dd9
DESKTOP-LD2HTVT\rhliu <1437604763@qq.com>
2022-09-10 17:43:56 +0800

    添加

--- src/pages/tabBar/document.vue
+++ src/pages/tabBar/document.vue
@@ -1,17 +1,62 @@
 <template>
-	<view class="content" @click="to">
-        document
-	</view>
+    <view class="document">
+        <view class="splie" v-for="(item,index) in data.view" :key="index" :style="item.type != 'editor'?{}:{flex:1}">
+            <view class="content">
+                <view class="header">
+                    123
+                </view>
+                <view class="main"></view>
+            </view>
+            <view v-if="item.type != 'editor'" class="vertical"></view>
+        </view>
+    </view>
 </template>
 
 
 <script setup lang="ts">
 import { onShow } from '@/source/hook/uniapp';
+import { reactive } from '@vue/composition-api';
+const data = reactive({
+    view: [{
+        type: 'folder'
+    }, {
+        type: 'folder'
+    }, {
+        type: 'editor'
+    }]
+});
 function to() {
-	uni.navigateTo({url:'/pages/index/home'})
+    uni.navigateTo({ url: '/pages/index/home' })
 }
 </script>
 
-<style>
+<style lang="scss">
+.document {
+    flex: 1;
+    flex-direction: row;
 
+    .splie {
+        flex-direction: row;
+        position: sticky;
+        border-right: 1px solid rgba($color: #000000, $alpha: 0.05);
+        .content {
+            flex: 1;
+
+            .header {
+                border-bottom: 1px solid rgba($color: #000000, $alpha: 0.05);
+            }
+
+            .main {
+                flex: 1;
+            }
+        }
+
+        .vertical {
+            width: 4px;
+            height: 100%;
+            position: absolute;
+            right: 0;
+        }
+    }
+}
 </style>
